a8ecd721f080292c8c2af33e201d539eacaf8d44
Chakistrikis <christian.aguilerayamal@gmail.com>
2025-05-23 15:38:25 -0600

    Feat: Added CRUD operations.

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -4,11 +4,32 @@ import com.platzi.market.persistence.crud.ProductCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductCrudRepository productCrudRepository;
 
     public List<Producto> getAll() {
         return (List<Producto>) productCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productCrudRepository.deleteById(idProducto);
     }
 }
